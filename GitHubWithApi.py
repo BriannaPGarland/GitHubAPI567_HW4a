3f8c8853d8cda85f26ae64969fad3ae1255f5c1f
Brianna Garland <56358388+BriannaPGarland@users.noreply.github.com>
2022-10-13 19:32:05 -0400

    Update GitHubWithApi.py

--- GitHubWithApi.py
+++ GitHubWithApi.py
@@ -7,25 +7,34 @@ from pprint import pprint
 # ID = input("What is your GitHub User ID?\n")
 # print(ID)
 
-
-
-def returnRepos(ID):
-    listOfFinal=[]
-    if ID != "": 
-        try:
+def get_usr_info(ID):
+    try:
             url = f"https://api.github.com/users/{ID}/repos"
             userRepoList = requests.get(url).json()
-        except: 
-            return "Sorry, User ID entered was not found. Please try again"
-        
-        else:
-            for i in range(len(userRepoList)):
-                repoName=userRepoList[i]["name"]
-                url = f"https://api.github.com/repos/{ID}/{repoName}/commits"
-                listOfCommits = requests.get(url).json()
-                numOfCommits=len(listOfCommits)
-                listOfFinal.append("Repo: "+repoName+" Number of commits: "+str(numOfCommits))
-                print("Repo: "+repoName+" Number of commits: "+str(numOfCommits))
-            return (listOfFinal)
+            return userRepoList
+    except: 
+        return "Sorry, User ID entered was not found. Please try again"
+    
+def get_repo_info(ID,repoName):
+    url = f"https://api.github.com/repos/{ID}/{repoName}/commits"
+    listOfCommits = requests.get(url).json()
+    return listOfCommits
+
+def returnRepos(ID):
+    listOfFinal =[]
+    if ID != "": 
+        userRepoList =get_usr_info(ID)
+        for i in range(len(userRepoList)):
+            repoName=userRepoList[i]["name"]
+           
+            listOfCommits = get_repo_info(ID, repoName)
+            
+            numOfCommits=len(listOfCommits)
+            listOfFinal.append("Repo: "+repoName+" Number of commits: "+str(numOfCommits))
+            print("Repo: "+repoName+" Number of commits: "+str(numOfCommits))
+        return (listOfFinal)
     else:
         return("Error: No user ID was entered")
+
+
+
